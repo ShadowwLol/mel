e4def1adb5ba227dc8cbb41dba3a21dbdd82bd9e
ShadowwLol <shadowwlol613@protonmail.com>
2021-12-14 23:21:51 +0000

    disabled threading

--- include/MEL_thread.h
+++ include/MEL_thread.h
@@ -4,14 +4,30 @@
 #if _WIN32
 #include <processthreadsapi.h>
 #include <synchapi.h>
+
+#define MEL_THREADED_FUNCTION LPTHREAD_START_ROUTINE
+
 #define MEL_Thread LPDWORD
-#define MEL_thread_create(thread_obj_ptr, function, args) CreateThread(NULL, 0, function, args, 0, thread_obj_ptr)
+#define MEL_thread_create(thread_obj, function, args) CreateThread(NULL, 0, function, args, 0, thread_obj)
 #define MEL_thread_join(thread_obj) WaitForSingleObject(thread_obj, INFINITE)
+
+#define MEL_Mutex HANDLE
+#define MEL_MUTEX_INIT CreateMutex(NULL, FALSE, NULL)
+#define MEL_mutex_lock(lock) WaitForSingleObject(lock, INFINITE)
+#define MEL_mutex_unlock(lock)
 #else
 #include <pthread.h>
+
+#define MEL_THREADED_FUNCTION void
+
 #define MEL_Thread pthread_t
-#define MEL_thread_create(thread_obj_ptr, function, args) pthread_create(thread_obj_ptr, NULL, function, args)
+#define MEL_thread_create(thread_obj, function, args) pthread_create(&thread_obj, NULL, function, args)
 #define MEL_thread_join(thread_obj) pthread_join(thread_obj, NULL)
+
+#define MEL_Mutex pthread_mutex_t
+#define MEL_MUTEX_INIT PTHREAD_MUTEX_INITIALIZER
+#define MEL_mutex_lock(lock) pthread_mutex_lock(lock)
+#define MEL_mutex_unlock(lock) pthread_mutex_unlock(lock)
 #endif
 
 #endif
